88e4a66f27e62fd168a3768e5d3fdfa3999066d1
HoangNguyen2k3 <nlqh2k3@gmail.com>
2023-11-28 22:15:12 +0700

    project begin

---
+++ fAccoutProfile.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace QuanlyquanCoffe
+{
+    partial class fAccoutProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txbUserName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txbDisplayName = new System.Windows.Forms.TextBox();
+            this.DisplayName = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txbPassWord = new System.Windows.Forms.TextBox();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.newPassWord = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.resetEnterPassWord = new System.Windows.Forms.Label();
+            this.btUpdate = new System.Windows.Forms.Button();
+            this.btExit = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txbUserName);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(12, 31);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(440, 63);
+            this.panel2.TabIndex = 1;
+            // 
+            // txbUserName
+            // 
+            this.txbUserName.Location = new System.Drawing.Point(187, 12);
+            this.txbUserName.Name = "txbUserName";
+            this.txbUserName.Size = new System.Drawing.Size(236, 22);
+            this.txbUserName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên đăng nhập:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txbDisplayName);
+            this.panel1.Controls.Add(this.DisplayName);
+            this.panel1.Location = new System.Drawing.Point(12, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(440, 63);
+            this.panel1.TabIndex = 2;
+            // 
+            // txbDisplayName
+            // 
+            this.txbDisplayName.Location = new System.Drawing.Point(187, 12);
+            this.txbDisplayName.Name = "txbDisplayName";
+            this.txbDisplayName.Size = new System.Drawing.Size(236, 22);
+            this.txbDisplayName.TabIndex = 1;
+            // 
+            // DisplayName
+            // 
+            this.DisplayName.AutoSize = true;
+            this.DisplayName.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisplayName.Location = new System.Drawing.Point(3, 12);
+            this.DisplayName.Name = "DisplayName";
+            this.DisplayName.Size = new System.Drawing.Size(129, 24);
+            this.DisplayName.TabIndex = 0;
+            this.DisplayName.Text = "Tên hiển thị:";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txbPassWord);
+            this.panel3.Controls.Add(this.lbPassword);
+            this.panel3.Location = new System.Drawing.Point(12, 169);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(440, 63);
+            this.panel3.TabIndex = 3;
+            // 
+            // txbPassWord
+            // 
+            this.txbPassWord.Location = new System.Drawing.Point(187, 12);
+            this.txbPassWord.Name = "txbPassWord";
+            this.txbPassWord.Size = new System.Drawing.Size(236, 22);
+            this.txbPassWord.TabIndex = 1;
+            this.txbPassWord.UseSystemPasswordChar = true;
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPassword.Location = new System.Drawing.Point(3, 12);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(104, 24);
+            this.lbPassword.TabIndex = 0;
+            this.lbPassword.Text = "Mật khẩu:";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.textBox1);
+            this.panel4.Controls.Add(this.newPassWord);
+            this.panel4.Location = new System.Drawing.Point(12, 238);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(440, 63);
+            this.panel4.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(187, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.UseSystemPasswordChar = true;
+            // 
+            // newPassWord
+            // 
+            this.newPassWord.AutoSize = true;
+            this.newPassWord.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newPassWord.Location = new System.Drawing.Point(3, 12);
+            this.newPassWord.Name = "newPassWord";
+            this.newPassWord.Size = new System.Drawing.Size(146, 24);
+            this.newPassWord.TabIndex = 0;
+            this.newPassWord.Text = "Mật khẩu mới:";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.textBox2);
+            this.panel5.Controls.Add(this.resetEnterPassWord);
+            this.panel5.Location = new System.Drawing.Point(12, 307);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(440, 63);
+            this.panel5.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(187, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(236, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // resetEnterPassWord
+            // 
+            this.resetEnterPassWord.AutoSize = true;
+            this.resetEnterPassWord.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetEnterPassWord.Location = new System.Drawing.Point(3, 12);
+            this.resetEnterPassWord.Name = "resetEnterPassWord";
+            this.resetEnterPassWord.Size = new System.Drawing.Size(93, 24);
+            this.resetEnterPassWord.TabIndex = 0;
+            this.resetEnterPassWord.Text = "Nhập lại:";
+            // 
+            // btUpdate
+            // 
+            this.btUpdate.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btUpdate.Location = new System.Drawing.Point(260, 376);
+            this.btUpdate.Name = "btUpdate";
+            this.btUpdate.Size = new System.Drawing.Size(90, 40);
+            this.btUpdate.TabIndex = 6;
+            this.btUpdate.Text = "Cập nhật";
+            this.btUpdate.UseVisualStyleBackColor = false;
+            // 
+            // btExit
+            // 
+            this.btExit.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btExit.Location = new System.Drawing.Point(356, 376);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(80, 40);
+            this.btExit.TabIndex = 7;
+            this.btExit.Text = "Thoát";
+            this.btExit.UseVisualStyleBackColor = false;
+            this.btExit.Click += new System.EventHandler(this.btExit_Click);
+            // 
+            // fAccoutProfile
+            // 
+            this.AcceptButton = this.btUpdate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btExit;
+            this.ClientSize = new System.Drawing.Size(467, 428);
+            this.Controls.Add(this.btExit);
+            this.Controls.Add(this.btUpdate);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "fAccoutProfile";
+            this.Text = "Thông tin cá nhân";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txbUserName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txbDisplayName;
+        private System.Windows.Forms.Label DisplayName;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txbPassWord;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label newPassWord;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label resetEnterPassWord;
+        private System.Windows.Forms.Button btUpdate;
+        private System.Windows.Forms.Button btExit;
+    }
+}
---
+++ fLogin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanlyquanCoffe
+{
+    public partial class fLogin : Form
+    {
+        public fLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            fTableManager f = new fTableManager();
+            f.ShowDialog();
+            this.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void fLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(MessageBox.Show("Bạn có muốn thoát chương trình ?","Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
