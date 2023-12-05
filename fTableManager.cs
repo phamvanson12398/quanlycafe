7143cc0ce767cd71dcda3ad34d38ae935a327ca7
HoangNguyen2k3 <nlqh2k3@gmail.com>
2023-12-05 10:56:04 +0700

    day 5

--- fTableManager.cs
+++ fTableManager.cs
@@ -5,10 +5,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Menu = QuanlyquanCoffe.DTO.Menu;
 
 namespace QuanlyquanCoffe
 {
@@ -27,7 +30,8 @@ namespace QuanlyquanCoffe
             {
                 Button btn = new Button() { Width = TableDAO.tableWidth, Height = TableDAO.tableHeight };
                 btn.Text = item.Name+Environment.NewLine+item.Status;
-              flpTable.Controls.Add(btn);
+                btn.Click += btn_Click;
+              btn.Tag = item;
                 switch (item.Status)
                 {
                     case "Trống": btn.BackColor = Color.Aqua;
@@ -35,10 +39,34 @@ namespace QuanlyquanCoffe
                     default: btn.BackColor = Color.LightPink; 
                         break;
                 }
+                flpTable.Controls.Add(btn);
             }
+        }
+        void ShowBill(int id)
+        {
+            lsvBill.Items.Clear();
+            List<QuanlyquanCoffe.DTO.Menu> listBillInfo=MenuDAO.Instance.GetListMenuByTable(id);
+            float totalprice = 0;
+            foreach (QuanlyquanCoffe.DTO.Menu item in listBillInfo)
+            {
+                ListViewItem lvsItem=new ListViewItem(item.FoodName.ToString());
+                lvsItem.SubItems.Add(item.Count.ToString());
+                lvsItem.SubItems.Add(item.Price.ToString());
+                lvsItem.SubItems.Add(item.TotalPrice.ToString());
+                totalprice+= item.TotalPrice;
+                lsvBill.Items.Add(lvsItem);
+            }
+            CultureInfo culture=new CultureInfo("vi-VN");
+            Thread.CurrentThread.CurrentCulture = culture;
+            txbTotalPrice.Text = totalprice.ToString("c",culture);
         }
         #endregion
         #region Events
+        void btn_Click(object sender, EventArgs e)
+        {
+            int TableId=((sender as Button).Tag as Table).ID;
+            ShowBill(TableId);
+        }
         private void ĐăngxuấtToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -56,8 +84,13 @@ namespace QuanlyquanCoffe
             f.ShowDialog();
         }
         #endregion
-
+       
         private void flpTable_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void fTableManager_Load(object sender, EventArgs e)
         {
 
         }
